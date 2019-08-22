824b7d7f43c833bec713b6550274c76c9755695a
Mohammed Sufiyan Farooq Budye <sufiyan.AnnantaSourcellc.onmicrosoft.com>
2019-08-22 15:25:28 +0530

    Added Ibu Collection project

---
+++ IbuCollection/IbuCollection/BLL/Helpers/ExcelToSqlExporter.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Web;
+
+namespace IbuCollection.BLL.Helpers
+{
+    public class ExcelToSqlExporter
+    {
+        private readonly string SqlConnectionString = "";
+
+        public void ExportExcelToSql()
+        {
+
+            //try
+            //{
+            //    var excel = new ExcelPackage(Upload.FileContent);
+            //    var dt = excel.ToDataTable();
+            //    var table = "Contacts";
+            //    using (var conn = new SqlConnection("Server=.;Database=Test;Integrated Security=SSPI"))
+            //    {
+            //        var bulkCopy = new SqlBulkCopy(conn);
+            //        bulkCopy.DestinationTableName = table;
+            //        conn.Open();
+            //        var schema = conn.GetSchema("Columns", new[] { null, null, table, null });
+            //        foreach (DataColumn sourceColumn in dt.Columns)
+            //        {
+            //            foreach (DataRow row in schema.Rows)
+            //            {
+            //                if (string.Equals(sourceColumn.ColumnName, (string)row["COLUMN_NAME"], StringComparison.OrdinalIgnoreCase))
+            //                {
+            //                    bulkCopy.ColumnMappings.Add(sourceColumn.ColumnName, (string)row["COLUMN_NAME"]);
+            //                    break;
+            //                }
+            //            }
+            //        }
+            //        bulkCopy.WriteToServer(dt);
+            //    }
+            //}
+            //catch (Exception ex)
+            //{
+
+            //    throw ex;
+            //}
+        }
+    }
+}
---
+++ IbuCollection/IbuCollection/Controllers/HomeController.cs
@@ -0,0 +1,127 @@
+﻿using OfficeOpenXml;
+using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Data.SqlClient;
+using System.IO;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace IbuCollection.Controllers
+{
+    public class HomeController : Controller
+    {
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public ActionResult Index(HttpPostedFileBase postedFile)
+        {
+            string filePath = string.Empty;
+            if (postedFile != null)
+            {
+                string path = Server.MapPath(@"~\Content\sheets");
+                if (!Directory.Exists(path))
+                {
+                    Directory.CreateDirectory(path);
+                }
+
+                filePath = path + Path.GetFileName(postedFile.FileName);
+                string extension = Path.GetExtension(postedFile.FileName);
+                postedFile.SaveAs(filePath);
+
+                string conString = string.Empty;
+                //switch (extension)
+                //{
+                //    case ".xls": //Excel 97-03.
+                //        conString = ConfigurationManager.ConnectionStrings["Excel03ConString"].ConnectionString;
+                //        break;
+                //    case ".xlsx": //Excel 07 and above.
+                //        conString = ConfigurationManager.ConnectionStrings["Excel07ConString"].ConnectionString;
+                //        break;
+                //}
+
+                FileInfo excel = new FileInfo(filePath);
+                using (var package = new ExcelPackage(excel))
+                {
+                    var workbook = package.Workbook;
+
+                    //*** Sheet 1
+                    var worksheet = workbook.Worksheets.First();
+
+                    string strConnString = @"Data Source=(localdb)\MSSQLLocalDB;Initial Catalog=IbuCollection;Integrated Security=True;Connect Timeout=30;Encrypt=False;TrustServerCertificate=False;ApplicationIntent=ReadWrite;MultiSubnetFailover=False";
+                    var objConn = new SqlConnection(strConnString);
+                    objConn.Open();
+
+                    //*** Loop to Insert
+                    int totalRows = worksheet.Dimension.End.Row;
+                    for (int i = 2; i <= totalRows; i++)
+                    {
+                        string strSQL = "INSERT INTO [dbo].[User] (Id,UserName,Email,Password,MobileNumber) "
+                            + " VALUES  ("
+                            + " '" + worksheet.Cells[i, 1].Text.ToString() + "', "
+                            + " '" + worksheet.Cells[i, 2].Text.ToString() + "', "
+                            + " '" + worksheet.Cells[i, 3].Text.ToString() + "', "
+                            + " '" + worksheet.Cells[i, 4].Text.ToString() + "', "
+                            + " '" + worksheet.Cells[i, 5].Text.ToString() + "' "
+                            + ")";
+                        var objCmd = new SqlCommand(strSQL, objConn);
+                        objCmd.ExecuteNonQuery();
+                    }
+
+                    objConn.Close();
+                }
+            }
+
+
+            return View();
+        }
+
+        public ActionResult About()
+        {
+            ViewBag.Message = "Your application description page.";
+            //D:\Projects\Git\Templates\AspNet\MVC\IbuCollection\IbuCollection\Content\sheets\ExcelUserData.xlsx
+            FileInfo excel = new FileInfo(Server.MapPath(@"~\Content\sheets\ExcelUserData.xlsx"));
+            using (var package = new ExcelPackage(excel))
+            {
+                var workbook = package.Workbook;
+
+                //*** Sheet 1
+                var worksheet = workbook.Worksheets.First();
+
+                string strConnString = @"Data Source=(localdb)\MSSQLLocalDB;Initial Catalog=IbuCollection;Integrated Security=True;Connect Timeout=30;Encrypt=False;TrustServerCertificate=False;ApplicationIntent=ReadWrite;MultiSubnetFailover=False";
+                var objConn = new SqlConnection(strConnString);
+                objConn.Open();
+
+                //*** Loop to Insert
+                int totalRows = worksheet.Dimension.End.Row;
+                for (int i = 2; i <= totalRows; i++)
+                {
+                    string strSQL = "INSERT INTO [dbo].[User] (Id,UserName,Email,Password,MobileNumber) "
+                        + " VALUES  ("
+                        + " '" + worksheet.Cells[i, 1].Text.ToString() + "', "
+                        + " '" + worksheet.Cells[i, 2].Text.ToString() + "', "
+                        + " '" + worksheet.Cells[i, 3].Text.ToString() + "', "
+                        + " '" + worksheet.Cells[i, 4].Text.ToString() + "', "
+                        + " '" + worksheet.Cells[i, 5].Text.ToString() + "' "
+                        + ")";
+                    var objCmd = new SqlCommand(strSQL, objConn);
+                    objCmd.ExecuteNonQuery();
+                }
+
+                objConn.Close();
+            }
+            return View();
+        }
+
+        public ActionResult Contact()
+        {
+            ViewBag.Message = "Your contact page.";
+
+            return View();
+        }
+    }
+}
